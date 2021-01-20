7af8c7e777301d4ea6bc21352f8d605597b97d98
unknown <krolczyk.artur@gmail.com>
2021-01-20 19:26:51 +0100

    first commit

---
+++ src/app.ts
@@ -0,0 +1,53 @@
+const autobind = (_: any, _2: string, description: PropertyDescriptor) =>{
+    const originalMethod = description.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+          const  boundFn = originalMethod.bind(this);
+            return boundFn
+        }
+    }
+    return adjDescriptor
+}
+
+class DragDropInput {
+    templateElement: HTMLTemplateElement; 
+    hostElement: HTMLDivElement;
+    element:HTMLFormElement;
+    titleInputElement: HTMLInputElement
+    descriptionInputElement: HTMLInputElement
+    peopleInputElement: HTMLInputElement
+
+    constructor() {
+        this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!;
+        this.hostElement = <HTMLDivElement>document.getElementById('app');
+
+        const importedNode = document.importNode(
+            this.templateElement.content, 
+            true
+            )
+        this.element = <HTMLFormElement>importedNode.firstElementChild
+        this.element.id = 'user-input'
+        this.titleInputElement = <HTMLInputElement>this.element.querySelector('#title')
+        this.descriptionInputElement = <HTMLInputElement>this.element.querySelector('#description')
+        this.peopleInputElement = <HTMLInputElement>this.element.querySelector('#people')
+
+
+this.configure();
+        this.attach();
+    }
+@autobind
+private submitHandler(event:Event){
+    event.preventDefault();
+    console.log(this.titleInputElement.value)
+}
+private configure () {
+    this.element.addEventListener('submit', this.submitHandler)
+}
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element)
+    }
+}
+
+const projectInput = new DragDropInput()
